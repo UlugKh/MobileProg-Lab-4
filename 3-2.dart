da1163d81ec4924e811d021bbd79a7642254a976
UlugKh <141512061+UlugKh@users.noreply.github.com>
2025-09-17 10:32:30 +0500

    Create 3-2.dart

---
+++ 3-2.dart
@@ -0,0 +1,26 @@
+class Animal {
+  void makeSound() {
+    print('Some generic animal sound');
+  }
+}
+
+class Dog extends Animal {
+  @override
+  void makeSound() {
+    print('Woof!');
+  }
+}
+
+class Cat extends Animal {
+  @override
+  void makeSound() {
+    print('Meow!');
+  }
+}
+
+void main() {
+  Animal dog = Dog();
+  Animal cat = Cat();
+  dog.makeSound();
+  cat.makeSound();
+}
